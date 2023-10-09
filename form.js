26fb074144db5064674e95a95a27e59c8e893105
Krisna Firdaus <krisnafirdaus@gmail.com>
2023-10-09 21:19:47 +0700

    fix bugs

--- form.js
+++ form.js
@@ -1,38 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    const nameInput = document.querySelector('.input-name');
-    const emailInput = document.querySelector('.input-email');
-    const messageInput = document.querySelector('.input-message');
-  
-    form.addEventListener('submit', async (e) => {
-      e.preventDefault(); // Mencegah pengiriman formulir default
-  
-      const formData = {
-        name: nameInput.value,
-        email: emailInput.value,
-        message: messageInput.value,
-      };
-  
-      try {
-        // Kirim data ke backend dengan menggunakan fetch atau metode lainnya
-        const response = await fetch('/api/submit', {
-          method: 'POST', // Metode HTTP
-          headers: {
-            'Content-Type': 'application/json', // Tipe konten adalah JSON
-          },
-          body: JSON.stringify(formData), // Mengonversi objek FormData menjadi JSON
-        });
-  
-        if (response.ok) {
-          // Form berhasil dikirim, lakukan tindakan yang sesuai (misalnya, tampilkan pesan sukses)
-          alert('Form berhasil dikirim!');
-        } else {
-          // Form gagal dikirim, tampilkan pesan kesalahan
-          alert('Terjadi kesalahan saat mengirim formulir.');
-        }
-      } catch (error) {
-        console.error('Terjadi kesalahan:', error);
+function postContactFormData(event) {
+  event.preventDefault();
+
+  // Ambil data dari form
+  const form = event.target;
+  const name = form.querySelector('[name="name"]').value;
+  const email = form.querySelector('[name="email"]').value;
+  const message = form.querySelector('[name="message"]').value;
+
+  // Buat objek data
+  const data = {
+      name: name,
+      email: email,
+      message: message
+  };
+
+  // Kirim data sebagai JSON
+  fetch('http://localhost:3000/form', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.success) {
+          alert('Terima kasih! Pesan Anda telah terkirim.');
+          form.reset();
+      } else {
+          alert('Maaf, terjadi kesalahan. Silakan coba lagi.');
       }
-    });
-  });
-  
+  })
+  .catch(error => console.error('Error sending contact data:', error));
+}
+
+
+// Event listener untuk form submission
+document.querySelector('form').addEventListener('submit', postContactFormData);
